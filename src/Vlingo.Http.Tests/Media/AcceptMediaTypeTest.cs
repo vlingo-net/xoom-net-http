8d3e74a8d6b10e426477f9dc021fbed34f927b01
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-08-27 23:01:46 +0200

    Add CompareTo to accept media type and unit tests

---
+++ src/Vlingo.Http.Tests/Media/AcceptMediaTypeTest.cs
@@ -0,0 +1,67 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Vlingo.Http.Media;
+using Xunit;
+
+namespace Vlingo.Http.Tests.Media
+{
+    public class AcceptMediaTypeTest
+    {
+        [Fact]
+        public void SpecificMimeTypeGreaterThanGeneric()
+        {
+            var acceptMediaType1 = new ResponseMediaTypeSelector.AcceptMediaType("application", "json");
+            var acceptMediaType2 = new ResponseMediaTypeSelector.AcceptMediaType("*", "*");
+            Assert.Equal( 1, acceptMediaType1.CompareTo(acceptMediaType2));
+            Assert.Equal( -1, acceptMediaType2.CompareTo(acceptMediaType1));
+        }
+        
+        [Fact]
+        public void SpecificMimeSubTypeGreaterThanGeneric()
+        {
+            var acceptMediaType1 = new ResponseMediaTypeSelector.AcceptMediaType("application", "json");
+            var acceptMediaType2 = new ResponseMediaTypeSelector.AcceptMediaType("application", "*");
+            Assert.Equal( 1, acceptMediaType1.CompareTo(acceptMediaType2));
+            Assert.Equal( -1, acceptMediaType2.CompareTo(acceptMediaType1));
+        }
+        
+        [Fact]
+        public void SpecificParameterGreaterThanGenericWithSameQualityFactor()
+        {
+            var acceptMediaType1 = new MediaTypeDescriptor.Builder<ResponseMediaTypeSelector.AcceptMediaType>(
+                    (a, b, c) => new ResponseMediaTypeSelector.AcceptMediaType(a, b, c))
+                .WithMimeType("application")
+                .WithMimeSubType("xml")
+                .WithParameter("version", "1.0")
+                .Build();
+
+            var acceptMediaType2 = new ResponseMediaTypeSelector.AcceptMediaType("application", "json");
+            Assert.Equal( 1, acceptMediaType1.CompareTo(acceptMediaType2));
+            Assert.Equal( -1, acceptMediaType2.CompareTo(acceptMediaType1));
+        }
+        
+        [Fact]
+        public void QualityFactorTrumpsSpecificity() {
+            var acceptMediaType1 = new MediaTypeDescriptor.Builder<ResponseMediaTypeSelector.AcceptMediaType>(
+                    (a, b, c) => new ResponseMediaTypeSelector.AcceptMediaType(a, b, c))
+                .WithMimeType("text")
+                .WithMimeSubType("*")
+                .Build();
+
+            var acceptMediaType2 = new MediaTypeDescriptor.Builder<ResponseMediaTypeSelector.AcceptMediaType>(
+                    (a, b, c) => new ResponseMediaTypeSelector.AcceptMediaType(a, b, c))
+                .WithMimeType("text")
+                .WithMimeSubType("json")
+                .WithParameter("q", "0.8")
+                .Build();
+
+            Assert.Equal( 1, acceptMediaType1.CompareTo(acceptMediaType2));
+            Assert.Equal( -1, acceptMediaType2.CompareTo(acceptMediaType1));
+        }
+    }
+}
--- src/Vlingo.Http/Media/ResponseMediaTypeSelector.cs
+++ src/Vlingo.Http/Media/ResponseMediaTypeSelector.cs
@@ -5,6 +5,7 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
+using System;
 using System.Collections.Generic;
 using Vlingo.Http.Resource;
 
@@ -12,7 +13,7 @@ namespace Vlingo.Http.Media
 {
     public class ResponseMediaTypeSelector
     {
-        private const char ACCEPT_MEDIA_TYPE_SEPARATOR = ',';
+        private const char AcceptMediaTypeSeparator = ',';
 
         private readonly SortedSet<AcceptMediaType> _responseMediaTypesByPriority;
         private readonly string _mediaTypeDescriptors;
@@ -26,7 +27,7 @@ namespace Vlingo.Http.Media
 
         private void ParseMediaTypeDescriptors(string contentTypeList)
         {
-            var acceptedContentTypeDescriptors = contentTypeList.Split(ACCEPT_MEDIA_TYPE_SEPARATOR);
+            var acceptedContentTypeDescriptors = contentTypeList.Split(AcceptMediaTypeSeparator);
 
             foreach (var acceptedContentTypeDescriptor in acceptedContentTypeDescriptors)
             {
@@ -53,29 +54,28 @@ namespace Vlingo.Http.Media
             throw new MediaTypeNotSupportedException(_mediaTypeDescriptors);
         }
 
-
-        private class AcceptMediaType : MediaTypeDescriptor, IComparer<AcceptMediaType>
+        internal class AcceptMediaType : MediaTypeDescriptor, IComparer<AcceptMediaType>, IComparable<AcceptMediaType>
         {
-            private const string MIME_TYPE_WILDCARD = "*";
-            private const string QUALITY_FACTOR_PARAMETER = "q";
-            private const float DEFAULT_QUALITY_FACTOR_VALUE = 1.0f;
+            private const string MimeTypeWildcard = "*";
+            private const string QualityFactorParameter = "q";
+            private const float DefaultQualityFactorValue = 1.0f;
 
             private readonly float _qualityFactor;
 
             public AcceptMediaType(string mimeType, string mimeSubType)
                 : base(mimeType, mimeSubType)
             {
-                _qualityFactor = DEFAULT_QUALITY_FACTOR_VALUE;
+                _qualityFactor = DefaultQualityFactorValue;
             }
 
             public AcceptMediaType(string mimeType, string mimeSubType, IDictionary<string, string> parameters)
                 : base(mimeType, mimeSubType, parameters)
             {
-                var qualityFactor = DEFAULT_QUALITY_FACTOR_VALUE;
+                var qualityFactor = DefaultQualityFactorValue;
 
-                if (parameters.ContainsKey(QUALITY_FACTOR_PARAMETER))
+                if (parameters.ContainsKey(QualityFactorParameter))
                 {
-                    if (float.TryParse(parameters[QUALITY_FACTOR_PARAMETER], out var f))
+                    if (float.TryParse(parameters[QualityFactorParameter], out var f))
                     {
                         qualityFactor = f;
                     }
@@ -86,6 +86,8 @@ namespace Vlingo.Http.Media
 
             public int Compare(AcceptMediaType x, AcceptMediaType y)
                 => -CompareForAscendingOrder(x, y);
+            
+            public int CompareTo(AcceptMediaType other) => CompareForAscendingOrder(this, other);
 
             private static int CompareForAscendingOrder(AcceptMediaType x, AcceptMediaType y)
             {
@@ -108,10 +110,8 @@ namespace Vlingo.Http.Media
                         return (y.IsGenericSubType() ? 1 : x.CompareParameters(y));
                     }
                 }
-                else
-                {
-                    return x._qualityFactor.CompareTo(y._qualityFactor);
-                }
+
+                return x._qualityFactor.CompareTo(y._qualityFactor);
             }
 
             private int CompareParameters(AcceptMediaType other) => Parameters.Count.CompareTo(other.Parameters.Count);
@@ -120,11 +120,9 @@ namespace Vlingo.Http.Media
                 => (IsGenericType() || string.Equals(MimeType, contentMediaType.MimeType))
                     && (IsGenericSubType() || string.Equals(MimeSubType, contentMediaType.MimeSubType));
 
-            private bool IsGenericSubType() => string.Equals(MimeSubType, MIME_TYPE_WILDCARD);
+            private bool IsGenericSubType() => string.Equals(MimeSubType, MimeTypeWildcard);
 
-            private bool IsGenericType() => string.Equals(MimeType, MIME_TYPE_WILDCARD);
-
-            
+            private bool IsGenericType() => string.Equals(MimeType, MimeTypeWildcard);
         }
     }
 }
