175c6e2348ec5dd0a138bcaa744e028a5c941a0a
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-08-28 14:21:39 +0200

    Implement Converter for Console log in tests

---
+++ src/Vlingo.Http.Tests/Converter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace Vlingo.Http.Tests
+{
+    public class Converter : TextWriter
+    {
+        ITestOutputHelper _output;
+        
+        public Converter(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+        
+        public override Encoding Encoding => Encoding.UTF8;
+
+        public override void WriteLine(string message)
+        {
+            try
+            {
+                _output.WriteLine(message);
+            }
+            catch (InvalidOperationException e)
+            {
+                if (e.Message != "There is no currently active test.")
+                {
+                    throw;
+                }
+            }
+        }
+
+        public override void WriteLine(string format, params object[] args) => _output.WriteLine(format, args);
+    }
+}
