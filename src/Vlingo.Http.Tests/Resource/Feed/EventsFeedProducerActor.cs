9b4f598120894c11f7772a0f36ec62f1fffd2670
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-08-31 18:20:02 +0200

    Implement EventsFeedProducerActor

---
+++ src/Vlingo.Http.Tests/Resource/Feed/EventsFeedProducerActor.cs
@@ -0,0 +1,34 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System.Text;
+using Vlingo.Actors;
+using Vlingo.Http.Resource.Feed;
+
+namespace Vlingo.Http.Tests.Resource.Feed
+{
+    public class EventsFeedProducerActor : Actor, IFeedProducer
+    {
+        public void ProduceFeedFor(FeedProductRequest request)
+        {
+            var body =
+                new StringBuilder()
+                    .Append(request.feedName)
+                    .Append(":")
+                    .Append(request.feedProductId)
+                    .Append(":");
+            
+            for (int count = 1; count <= request.feedProductElements; ++count)
+            {
+                body.Append(count).Append("\n");
+            }
+            
+            var response = Response.Of(Response.ResponseStatus.Ok, body.ToString());
+            request.context.Completes.With(response);
+        }
+    }
+}
