62feb4bcbcd06f95107b2c21a3ce717aa53e5408
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-12-21 15:26:53 +0100

    Add ServerBootstrap

---
+++ src/Vlingo.Http.Tests/Resource/ServerBootstrap.cs
@@ -0,0 +1,64 @@
+// Copyright © 2012-2020 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using Vlingo.Actors;
+using Vlingo.Http.Resource;
+using Vlingo.Http.Tests.Sample.User;
+using Configuration = Vlingo.Http.Resource.Configuration;
+
+namespace Vlingo.Http.Tests.Resource
+{
+    public class ServerBootstrap
+    {
+        public static ServerBootstrap Instance { get; private set; }
+        
+        public World World { get; }
+        
+        public static void Main(string[] args)
+        {
+            Instance = new ServerBootstrap();
+        }
+        
+        public IServer Server { get; }
+        
+        private ServerBootstrap()
+        {
+            World = World.Start("vlingo-http-server");
+
+            var userResource = new UserResourceFluent(World);
+            var profileResource = new ProfileResourceFluent(World);
+            var r1 = userResource.Routes();
+            var r2 = profileResource.Routes();
+            var resources = Resources.Are(r1, r2);
+
+            Server =
+                Server.StartWith(
+                    World.Stage,
+                    resources,
+                    Filters.None(),
+                    8081,
+                    Configuration.SizingConf.DefineWith(4, 10, 100, 10240),
+                    Configuration.TimingConf.DefineWith(3, 1, 100),
+                    "arrayQueueMailbox",
+                    "arrayQueueMailbox");
+            
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                if (Instance != null)
+                {
+                    Instance.Server.Stop();
+
+                    Console.WriteLine("\n");
+                    Console.WriteLine("==============================");
+                    Console.WriteLine("Stopping vlingo/http Server...");
+                    Console.WriteLine("==============================");
+                }
+            };
+        }
+    }
+}
