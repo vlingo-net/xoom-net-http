886c4a31b7dec75bffb3927b4f28ef3c28abf640
Tomasz Jaskula <thomasv1000@hotmail.fr>
2020-01-19 20:50:41 +0100

    Implement ResourceErrorProcessor

--- src/Vlingo.Http/Resource/ResourceErrorProcessor.cs
+++ src/Vlingo.Http/Resource/ResourceErrorProcessor.cs
@@ -12,9 +12,24 @@ namespace Vlingo.Http.Resource
 {
     public class ResourceErrorProcessor
     {
-        public static Response ResourceHandlerError(IErrorHandler errorHandler, ILogger logger, Exception p2)
+        public static Response DefaultErrorResponse() => Response.Of(Response.ResponseStatus.InternalServerError);
+
+        public static Response ResourceHandlerError(IErrorHandler errorHandler, ILogger logger, Exception exception)
         {
-            throw new NotImplementedException();
+            Response response;
+            try
+            {
+                logger.Error("Exception thrown by Resource execution", exception);
+                response = errorHandler != null ?
+                    errorHandler.Handle(exception) :
+                    DefaultErrorHandler.Instance.Handle(exception);
+            }
+            catch
+            {
+                logger.Error("Exception thrown by error handler when handling error", exception);
+                response = DefaultErrorResponse();
+            }
+            return response;
         }
     }
 }
