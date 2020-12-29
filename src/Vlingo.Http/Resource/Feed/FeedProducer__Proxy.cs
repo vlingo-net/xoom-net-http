0f97b6820d78c4e55f069a3d3f34dd1e22843d1c
Tomasz Jaskula <thomasv1000@hotmail.fr>
2020-12-29 23:14:48 +0100

    Add FeedProducer__Proxy

---
+++ src/Vlingo.Http/Resource/Feed/FeedProducer__Proxy.cs
@@ -0,0 +1,43 @@
+// Copyright (c) 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using Vlingo.Actors;
+
+namespace Vlingo.Http.Resource.Feed
+{
+    public class FeedProducer__Proxy : IFeedProducer
+    {
+        private const string ProduceFeedForRepresentation1 =
+            "ProduceFeedFor(Vlingo.Http.Resource.Feed.FeedProductRequest)";
+
+        private readonly Actor _actor;
+        private readonly IMailbox _mailbox;
+
+        public FeedProducer__Proxy(Actor actor, IMailbox mailbox)
+        {
+            _actor = actor;
+            _mailbox = mailbox;
+        }
+
+        public void ProduceFeedFor(FeedProductRequest request)
+        {
+            if (!_actor.IsStopped)
+            {
+                Action<IFeedProducer> cons1617155091 = __ => __.ProduceFeedFor(request);
+                if (_mailbox.IsPreallocated)
+                    _mailbox.Send(_actor, cons1617155091, null, ProduceFeedForRepresentation1);
+                else
+                    _mailbox.Send(new LocalMessage<IFeedProducer>(_actor, cons1617155091, ProduceFeedForRepresentation1));
+            }
+            else
+            {
+                _actor.DeadLetters?.FailedDelivery(new DeadLetter(_actor, ProduceFeedForRepresentation1));
+            }
+        }
+    }
+}
