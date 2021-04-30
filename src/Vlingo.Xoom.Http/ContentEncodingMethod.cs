e761745bd8700d32a5bd4e2b87ed51a4d4d9e445
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-04-30 22:00:51 +0200

    Implement ContentEncodingMethod

---
+++ src/Vlingo.Xoom.Http/ContentEncodingMethod.cs
@@ -0,0 +1,33 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using Vlingo.Xoom.Common;
+
+namespace Vlingo.Xoom.Http
+{
+    public enum ContentEncodingMethod
+    {
+        Gzip,
+        Compress,
+        Deflate,
+        Brotli
+    }
+
+    public static class ContentEncodingMethodHelper
+    {
+        public static Optional<ContentEncodingMethod> Parse(string value)
+        {
+            if (Enum.TryParse(value, out ContentEncodingMethod parsed))
+            {
+                return Optional.Of(parsed);
+            }
+            
+            return Optional.Empty<ContentEncodingMethod>();
+        }
+    }
+}
