862cbe3d4b5c9afd5a676d7ed7da3765b4f789c0
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-05-02 22:10:17 +0200

    Implement ContentEncoding

--- src/Vlingo.Xoom.Http/ContentEncoding.cs
+++ src/Vlingo.Xoom.Http/ContentEncoding.cs
@@ -5,10 +5,33 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
+using System.Linq;
+
 namespace Vlingo.Xoom.Http
 {
+    /// <summary>
+    /// Contains the ordered list of content encodings that have been applied to a piece of
+    /// content.  To reverse any such compression, these encodings should be applied in reverse order
+    /// </summary>
     public class ContentEncoding
     {
+        public ContentEncodingMethod[] EncodingMethods { get; }
+
+        public ContentEncoding(params ContentEncodingMethod[] encodingMethods) => EncodingMethods = encodingMethods;
+
+        public ContentEncoding() => EncodingMethods = new ContentEncodingMethod[0];
+
+        public static ContentEncoding ParseFromHeader(string headerValue)
+        {
+            var methods = headerValue.Split(',');
+            var parsedMethods = methods.Select(ContentEncodingMethodHelper.Parse)
+                .Where(o => o.IsPresent)
+                .Select(o => o.Get())
+                .ToArray();
+
+            return new ContentEncoding(parsedMethods);
+        }
         
+        public static ContentEncoding None() => new ContentEncoding();
     }
 }
