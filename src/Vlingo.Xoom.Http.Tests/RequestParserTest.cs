9085a7e10b1bab68b3431853398bc8a43cbf577e
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-12-04 13:14:19 +0100

    Move DateExtensions from Http project to Common

--- src/Vlingo.Xoom.Http.Tests/RequestParserTest.cs
+++ src/Vlingo.Xoom.Http.Tests/RequestParserTest.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Vlingo.Xoom.Common;
 using Vlingo.Xoom.Http.Tests.Resource;
 using Xunit;
 using Xunit.Abstractions;
--- src/Vlingo.Xoom.Http.Tests/ResponseParserTest.cs
+++ src/Vlingo.Xoom.Http.Tests/ResponseParserTest.cs
@@ -7,6 +7,7 @@
 
 using System.Collections.Generic;
 using System.Text;
+using Vlingo.Xoom.Common;
 using Vlingo.Xoom.Http.Tests.Resource;
 using Xunit;
 using Xunit.Abstractions;
